95194732da7543ba0cdca715cbcdf3d7252d0331
iamanggi <anggipuspita452@gmail.com>
2024-10-16 21:27:47 +0700

    Menambahkan komponen Image dengan logo dan menambahkan ukuran serta padding

--- app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_123.ui.theme.QuestBasicComposable_123Theme
 
@@ -45,6 +49,11 @@ fun BasicComposable(modifier: Modifier = Modifier){
             fontWeight = FontWeight.Bold)
         )
         Text(text = "Halaman Login")
+        Image(painter = painterResource(
+            id = R.drawable.logo_umy
+        ),
+            contentDescription = null,
+            modifier = Modifier.padding(top = 40.dp).size(250.dp))
     }
 }
 
