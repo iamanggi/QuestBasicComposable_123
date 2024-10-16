e58584627bd95462c37478ca9cd72e1c4c379879
iamanggi <anggipuspita452@gmail.com>
2024-10-16 21:06:08 +0700

    Menambahkan padding pada komponen BasicLayout menggunakan inner padding

--- app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_123Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BasicComposable(modifier = Modifier.padding(innerPadding))
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestBasicComposable_123Theme {
-        Greeting("Android")
-    }
-}
