6d7741ccac1a21597eb612f2d607f5c9d29209df
iamanggi <anggipuspita452@gmail.com>
2024-10-16 21:16:05 +0700

    mengatur colomn agar elemen di dalamnya ter-align secara horizontal di tengah dan disusun secara vertikal di tengah

--- app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_123.ui.theme.QuestBasicComposable_123Theme
@@ -30,5 +33,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComposable(modifier: Modifier = Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
+    }
 }
+
