f9a641f9b7e2c676960a5bcb626cffc7480ff484
iamanggi <anggipuspita452@gmail.com>
2024-10-16 21:19:22 +0700

    Menambahkan style untuk TextStyle dengan ukuran font 50.sp dan ketebalan font Bold pada Text Login

--- app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_123/MainActivity.kt
@@ -13,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_123.ui.theme.QuestBasicComposable_123Theme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +40,10 @@ fun BasicComposable(modifier: Modifier = Modifier){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-        Text(text = "Login" )
+        Text(text = "Login",
+            style = TextStyle(fontSize = 50.sp,
+            fontWeight = FontWeight.Bold)
+        )
     }
 }
 
